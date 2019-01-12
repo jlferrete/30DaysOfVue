e31bd859f20bd11dc7434dde5e2ed907f612953c
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-01-12 08:50:44 +0100

    Day 4: V-Bind-Example

---
+++ day-04/v-bind-example/main.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Hello World!',
+    user: 'José Luis Ferrete',
+    city: 'Seville',
+    cityImage: 'https://sevillasecreta.co/wp-content/uploads/2016/05/Sevilla2.jpg'
+  }
+});
